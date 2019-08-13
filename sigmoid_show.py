2715de81c48ff5566ca0b5a88d3ab9dd5c916f17
xuezechao <xuezechao@360.cn>
2019-08-13 18:56:11 +0800

    使用python库  xzc_tools

--- sigmoid_show.py
+++ sigmoid_show.py
@@ -2,17 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import math
-import numpy as np
+from xzc_tools import tools
 from matplotlib import pyplot as plt
 
-x = [i for i in range(-10, 11)]
+@tools.funcRunTime
+def sigmoid_show():
+    try:
+        x = [i for i in range(-10, 11)]
+        y = [1 / (1 + math.exp(-i)) for i in x]
+        plt.title("sigmoid")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.plot(x, y)
+        plt.show()
+    except Exception as msg:
+        tools.printInfo(2, msg)
 
-y = [1/(1 + math.exp(-i)) for i in x]
-
-plt.title("sigmoid")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.plot(x,y)
-plt.show()
-
-
+if __name__ == '__main__':
+    sigmoid_show()
